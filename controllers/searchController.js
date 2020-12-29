ecf1300dd2e8d35887461c95e6e64150ba46c58e
Alvin <alvinzcoxiv@gmail.com>
2020-12-28 22:33:25 -0500

    wrote contoller that queries API for articles based on user parameters and returns the results to the front-end

---
+++ controllers/searchController.js
@@ -0,0 +1,47 @@
+const axios = require("axios");
+
+// Create variable for base URL and TAG
+const BASEURL = "http://hn.algolia.com/api/v1/search?query=";
+const TAG = "&tags=story";
+
+// Export object with search method that will return any articles with the search parameter in the title from Hacker News Algolia
+module.exports = {
+    search: function (req, res) {
+        console.log("searching articles");
+        console.log("search parameter", req.params.search);
+
+        // create variable with full URL
+        const FULLURL = BASEURL + req.params.search + TAG;
+        console.log(FULLURL);
+
+        // Use axios to query Hacker News Algolia
+        axios.get(FULLURL)
+            .then(result => {
+                console.log("initial result", result);
+
+                // create array that contains all results
+                const resultArry = result.hits;
+
+                // map resultArry to create a respone array that contains only the information that is to be sent back to the user
+                const responseArry = resultArry.map((index) => {
+                    return (
+                        {
+                            id: index.objectID,
+                            title: index.title,
+                            author: index.author,
+                            url: index.url
+                        }
+                    );
+                });
+                console.log("response array", responseArry)
+
+                // send the response arrry back to the front-end
+                res.json(responseArry)
+            })
+            // catch error and send 422 status error back to front-end
+            .catch(err => {
+                res.status(422).json(err);
+                console.log(err);
+            });
+    }
+};
--- routes/api/search.js
+++ routes/api/search.js
@@ -1,8 +1,8 @@
 const router = require("express").Router();
 const searchController = require("../../controllers/searchController");
 
-// Route to controller that is responsible for API call to Hacker Newa Algolia
+// Route to controller that is responsible for API get call to Hacker Newa Algolia
 router.route(":search")
     .get(searchController.search);
 
-module.expors = router
+module.expors = router;
