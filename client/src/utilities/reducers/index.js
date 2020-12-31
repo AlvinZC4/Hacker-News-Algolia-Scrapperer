676ec5ced9b74ddd1841d94e97128d8e600b414e
Alvin <alvinzcoxiv@gmail.com>
2020-12-30 22:39:44 -0500

    updated reducer by adding cases for action types

--- client/src/utilities/reducers/index.js
+++ client/src/utilities/reducers/index.js
@@ -1,3 +1,6 @@
+// Import constants from action-types.js to use as actions
+import { ADD_ARTICLES, UPDATE_SEARCHFIELD } from "../constants/action-types";
+
 // Set the initial state of the application.
 const initialState = {
     search = "",
@@ -5,7 +8,14 @@ const initialState = {
 };
 // Create the top level reducer for the application.
 function rootReducer(state = initialState, action) {
-    return state;
+    switch (action.type) {
+        case ADD_ARTICLES:
+            return {...state, results: action.payload};
+        case UPDATE_SEARCHFIELD:
+            return {...state, search: action.payload};
+        default:
+            return state;
+    };
 };
 
 export default rootReducer;
